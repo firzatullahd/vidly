affcbb6031d6e814412ecc499e36722c0e2b26b0
Firza17 <firzatullahdwikoramadhan@gmail.com>
2020-08-11 14:24:15 +0700

    Updates

---
+++ precache-manifest.f2fa8fc62cde1b6493e44d7b039ecd4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3328271d8a9b4561159a2e57a870479a",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "b61baad3dad43377c1a1",
+    "url": "/vidly/static/css/2.f3f2305b.chunk.css"
+  },
+  {
+    "revision": "eeffb88b81b7dca979f3",
+    "url": "/vidly/static/css/main.1dd7ad0a.chunk.css"
+  },
+  {
+    "revision": "b61baad3dad43377c1a1",
+    "url": "/vidly/static/js/2.8e83d0b3.chunk.js"
+  },
+  {
+    "revision": "2a34a1dc554c0f590228002c443969db",
+    "url": "/vidly/static/js/2.8e83d0b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeffb88b81b7dca979f3",
+    "url": "/vidly/static/js/main.c4aba9b0.chunk.js"
+  },
+  {
+    "revision": "cb388922157c702ba0d5",
+    "url": "/vidly/static/js/runtime-main.f8720289.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
